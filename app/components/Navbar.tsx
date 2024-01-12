c469e5da15c4eab6e368a6c2ffea561fe22527a2
123ZOKzok <zippyondiba@gmail.com>
2024-01-12 11:04:59 +0300

    ft add components

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="border-2 border-red-500 flexBetween mx-container padding-container relative z-30 py-5">
+      Navbar
+    </nav>
+  )
+}
